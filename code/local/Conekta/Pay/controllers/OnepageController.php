75097d43cc14c344a36d66426d9abe52631cbc06
MauricioMurga <mauricio_murga@hotmail.com>
2014-01-21 15:44:38 -0600

    pay completed

--- code/local/Conekta/Pay/controllers/OnepageController.php
+++ code/local/Conekta/Pay/controllers/OnepageController.php
@@ -45,11 +45,100 @@ class Conekta_Pay_OnepageController extends Mage_Checkout_OnepageController
 		$grandTotal = $quote->getGrandTotal();
 		$exploded_val=explode(".",$grandTotal);
 		$exploded_val=$exploded_val['0']*100;
-		
 		require(dirname(__FILE__) . '/../../conekta-php/lib/Conekta.php');
 		Conekta::setApiKey($key);
-		$charge = Conekta_Charge::create(array('description'=>'Conekta Pay','reference_id'=>$token_id,'amount'=>$exploded_val,'currency'=>$currency,'card'=>$token_id));
-		parent::saveOrderAction();
+		$s_info = Mage::getSingleton('checkout/session')->getQuote()->getShippingAddress()->getData();
+		$b_info = Mage::getSingleton('checkout/session')->getQuote()->getBillingAddress()->getData();
+		$p_info = $this->getOnepage()->getQuote()->getItemsCollection();
+		$n_items = count($p_info->getColumnValues('sku'));
+		$line_items = array();
+		for ($i = 0; $i < $n_items; $i ++) {
+			$name = $p_info->getColumnValues('name');
+			$name = $name[$i];
+			$sku = $p_info->getColumnValues('sku');
+			$sku = $sku[$i];
+			$price = $p_info->getColumnValues('price');
+			$price = $price[$i];
+			$description = $p_info->getColumnValues('description');
+			$description = $description[$i];
+			$product_type = $p_info->getColumnValues('product_type');
+			$product_type = $product_type[$i];
+			$line_items = array_merge($line_items, array(array(
+				"name"=>$name,
+				"sku"=>$sku,
+				"unit_price"=> $price,
+				"description"=>$description,
+				"quantity"=> 1,
+				"type"=>$product_type
+			  ))
+			);
+		}
+		$shipment = array();
+		if ($s_info['grand_total'] > 0) {
+			$shipment = array(
+			  "carrier"=>"estafeta",
+			  "service"=>"international",
+			  "tracking_id"=>"XXYYZZ-9990000",
+			  "price"=> $s_info['grand_total'],
+			  "address"=> array(
+				"street1"=>"250 Alexis St",
+				"city"=>$s_info['city'],
+				"state"=>$s_info['region'],
+				"country"=>$s_info['country_id'],
+				"zip"=>$s_info['postcode'],
+			  )
+			);
+		}
+		try {
+			$charge = Conekta_Charge::create(array(
+			  "description"=>"Compra en Magento de " . $b_info['email'],
+			  "amount"=> $exploded_val,
+			  "currency"=> $currency,
+			  "card"=> $token_id,
+			  "details"=> array(
+				"name"=> preg_replace('!\s+!', ' ', $b_info['firstname'] . ' ' . $b_info['middlename'] . ' ' . $b_info['firstname']),
+				"email"=> $b_info['email'],
+				"phone"=> $b_info['telephone'],
+				"billing_address"=> array(
+				  "company_name"=> $b_info['company'],
+				  "street1"=> $b_info['street'],
+				  "city"=>$b_info['city'],
+				  "state"=>$b_info['region'],
+				  "country"=>$b_info['country_id'],
+				  "zip"=>$b_info['postcode'],
+				  "phone"=>$b_info['telephone'],
+				  "email"=>$b_info['email']
+				),
+				"line_items"=> $line_items
+				),
+				"shipment"=> $shipment
+			  )
+			);
+			parent::saveOrderAction();
+		} catch (Conekta_Error $e) {
+			Mage::logException($e);
+            Mage::helper('checkout')->sendPaymentFailedEmail($this->getOnepage()->getQuote(), $e->getMessage());
+            $result['success'] = false;
+            $result['error'] = true;
+            $result['error_messages'] = $e->getMessage();
+
+            if ($gotoSection = $this->getOnepage()->getCheckout()->getGotoSection()) {
+                $result['goto_section'] = $gotoSection;
+                $this->getOnepage()->getCheckout()->setGotoSection(null);
+            }
+
+            if ($updateSection = $this->getOnepage()->getCheckout()->getUpdateSection()) {
+                if (isset($this->_sectionUpdateFunctions[$updateSection])) {
+                    $updateSectionFunction = $this->_sectionUpdateFunctions[$updateSection];
+                    $result['update_section'] = array(
+                        'name' => $updateSection,
+                        'html' => $this->$updateSectionFunction()
+                    );
+                }
+                $this->getOnepage()->getCheckout()->setUpdateSection(null);
+            }
+		}
+		$this->getResponse()->setBody(Mage::helper('core')->jsonEncode($result));
 	}
 
 }
