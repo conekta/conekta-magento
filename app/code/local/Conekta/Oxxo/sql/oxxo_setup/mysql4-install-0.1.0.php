ddddb8fc7d818e80d7e78e9f67d61480baf5f4be
MauricioMurga <mauricio_murga@hotmail.com>
2014-01-28 13:06:53 -0600

    oxxo completed

--- app/code/local/Conekta/Oxxo/sql/oxxo_setup/mysql4-install-0.1.0.php
+++ app/code/local/Conekta/Oxxo/sql/oxxo_setup/mysql4-install-0.1.0.php
@@ -5,11 +5,10 @@ $installer->startSetup();
 $installer->run("
 ALTER TABLE `{$installer->getTable('sales/quote_payment')}` ADD `codigo` VARCHAR( 255 ) NOT NULL;
 ALTER TABLE `{$installer->getTable('sales/quote_payment')}` ADD `codigo_barras` VARCHAR( 255 ) NOT NULL;
-ALTER TABLE `{$installer->getTable('sales/quote_payment')}` ADD `referencia` VARCHAR( 255 ) NOT NULL;
+
 
 ALTER TABLE `{$installer->getTable('sales/order_payment')}` ADD `codigo` VARCHAR( 255 ) NOT NULL ;
 ALTER TABLE `{$installer->getTable('sales/order_payment')}` ADD `codigo_barras` VARCHAR( 255 ) NOT NULL ;
-ALTER TABLE `{$installer->getTable('sales/order_payment')}` ADD `referencia` VARCHAR( 255 ) NOT NULL ;
 
 ");
 $installer->endSetup();
