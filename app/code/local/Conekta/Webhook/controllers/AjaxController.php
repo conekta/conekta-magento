1b8b22bcfef377a6d5b7d002a6f1035aa7e97381
MauricioMurga <mauricio_murga@hotmail.com>
2014-07-03 12:25:56 -0500

    update webhook listener to make invoices for offline payments #14371736802669

--- app/code/local/Conekta/Webhook/controllers/AjaxController.php
+++ app/code/local/Conekta/Webhook/controllers/AjaxController.php
@@ -7,11 +7,18 @@ class Conekta_Webhook_AjaxController extends Mage_Core_Controller_Front_Action {
 				$line_items = $charge->details->line_items;
 				$order = Mage::getModel('sales/order')->loadByIncrementId($charge->reference_id);
 				if (strpos($event->type, "charge.paid") !== false && $order->getId()) {
-						if ($order->getState() !== Mage_Sales_Model_Order::STATE_COMPLETE) {
-							$order->addStatusToHistory(Mage_Sales_Model_Order::STATE_COMPLETE);
-							$order->setData('state', Mage_Sales_Model_Order::STATE_COMPLETE);
-							$order->save();
-						}
+                        if ($order->hasInvoices() != true) {
+                                $invoice = $order->prepareInvoice();
+                                $invoice->register();
+                                Mage::getModel('core/resource_transaction')
+                                         ->addObject($invoice)
+                                         ->addObject($invoice->getOrder())
+                                         ->save();
+                                $invoice->sendEmail(true, '');
+                        }                            
+                        $order->addStatusToHistory(Mage_Sales_Model_Order::STATE_COMPLETE);
+                        $order->setData('state', Mage_Sales_Model_Order::STATE_COMPLETE);
+                        $order->save();
 				}		
 	}
 }
