d788180a310fb7bb78ced654be68d8ef02ddafde
MauricioMurga <mauricio_murga@hotmail.com>
2014-07-03 12:20:18 -0500

    change order status logic for offline payments #14371736802669 and send email data to conekta

--- app/code/local/Conekta/Oxxo/Model/Observer.php
+++ app/code/local/Conekta/Oxxo/Model/Observer.php
@@ -9,77 +9,78 @@ class Conekta_Oxxo_Model_Observer{
         if($event->payment->getMethod() == Mage::getModel('Conekta_Oxxo_Model_Oxxo')->getCode()){
             Conekta::setApiKey(Mage::getStoreConfig('payment/oxxo/privatekey'));
             $billing = $event->payment->getOrder()->getBillingAddress()->getData();
+            $email = $event->payment->getOrder()->getEmail();
             if ($event->payment->getOrder()->getShippingAddress()) {
-							$shipping = $event->payment->getOrder()->getShippingAddress()->getData();
-						}
+                $shipping = $event->payment->getOrder()->getShippingAddress()->getData();
+            }
             $items_collection = $event->payment->getOrder()->getItemsCollection(array(), true);
-						$line_items = array();
-						for ($i = 0; $i < count($items_collection->getColumnValues('sku')); $i ++) {
-							$name = $items_collection->getColumnValues('name');
-							$name = $name[$i];
-							$sku = $items_collection->getColumnValues('sku');
-							$sku = $sku[$i];
-							$price = $items_collection->getColumnValues('price');
-							$price = $price[$i];
-							$description = $items_collection->getColumnValues('description');
-							$description = $description[$i];
-							$product_type = $items_collection->getColumnValues('product_type');
-							$product_type = $product_type[$i];
-							$line_items = array_merge($line_items, array(array(
-								'name' => $name,
-								'sku' => $sku,
-								'unit_price' => $price,
-								'description' =>$description,
-								'quantity' => 1,
-								'type' => $product_type
-								))
-							);
-						}
-						$shipp = array();
-						if (empty($shipping) != true) {
-							$shipp = array(
-								'price' => $shipping['grand_total'],
-								'address' => array(
-								'street1' => $shipping['street'],
-								'city' => $shipping['city'],
-								'state' => $shipping['region'],
-								'country' => $shipping['country_id'],
-								'zip' => $shipping['postcode'],
-								'phone' =>$shipping['telephone'],
-								'email' =>$shipping['email']
-								)
-							);
-						}
+            $line_items = array();
+            for ($i = 0; $i < count($items_collection->getColumnValues('sku')); $i ++) {
+                $name = $items_collection->getColumnValues('name');
+                $name = $name[$i];
+                $sku = $items_collection->getColumnValues('sku');
+                $sku = $sku[$i];
+                $price = $items_collection->getColumnValues('price');
+                $price = $price[$i];
+                $description = $items_collection->getColumnValues('description');
+                $description = $description[$i];
+                $product_type = $items_collection->getColumnValues('product_type');
+                $product_type = $product_type[$i];
+                $line_items = array_merge($line_items, array(array(
+                    'name' => $name,
+                    'sku' => $sku,
+                    'unit_price' => $price,
+                    'description' =>$description,
+                    'quantity' => 1,
+                    'type' => $product_type
+                    ))
+                );
+            }
+            $shipp = array();
+            if (empty($shipping) != true) {
+                $shipp = array(
+                    'price' => $shipping['grand_total'],
+                    'address' => array(
+                    'street1' => $shipping['street'],
+                    'city' => $shipping['city'],
+                    'state' => $shipping['region'],
+                    'country' => $shipping['country_id'],
+                    'zip' => $shipping['postcode'],
+                    'phone' =>$shipping['telephone'],
+                    'email' =>$email
+                    )
+                );
+            }
             try {
-								$charge = Conekta_Charge::create(array(
-										'cash'=>array(
-												'type'=>'oxxo'
-										),
-										'amount' => intval(((float) $event->payment->getOrder()->grandTotal) * 100),
-										'description' => 'Compra en Magento',
-										'reference_id' => $event->payment->getOrder()->getIncrementId(),
-										'details' => array(
-											'name' => preg_replace('!\s+!', ' ', $billing['firstname'] . ' ' . $billing['middlename'] . ' ' . $billing['firstname']),
-											'email' => $billing['email'],
-											'phone' => $billing['telephone'],
-											'billing_address' => array(
-												'company_name' => $billing['company'],
-												'street1' => $billing['street'],
-												'city' =>$billing['city'],
-												'state' =>$billing['region'],
-												'country' =>$billing['country_id'],
-												'zip' =>$billing['postcode'],
-												'phone' =>$billing['telephone'],
-												'email' =>$billing['email']
-											),
-											'line_items' => $line_items,
-											'shipment' => $shipp
-											)
-										)
-								);
-						} catch (Conekta_Error $e){
-							throw new Mage_Payment_Model_Info_Exception($e->getMessage());
-						}
+                    $charge = Conekta_Charge::create(array(
+                            'cash'=>array(
+                                    'type'=>'oxxo'
+                            ),
+                            'amount' => intval(((float) $event->payment->getOrder()->grandTotal) * 100),
+                            'description' => 'Compra en Magento',
+                            'reference_id' => $event->payment->getOrder()->getIncrementId(),
+                            'details' => array(
+                                'name' => preg_replace('!\s+!', ' ', $billing['firstname'] . ' ' . $billing['middlename'] . ' ' . $billing['firstname']),
+                                'email' => $email,
+                                'phone' => $billing['telephone'],
+                                'billing_address' => array(
+                                    'company_name' => $billing['company'],
+                                    'street1' => $billing['street'],
+                                    'city' =>$billing['city'],
+                                    'state' =>$billing['region'],
+                                    'country' =>$billing['country_id'],
+                                    'zip' =>$billing['postcode'],
+                                    'phone' =>$billing['telephone'],
+                                    'email' =>$email
+                                ),
+                                'line_items' => $line_items,
+                                'shipment' => $shipp
+                                )
+                            )
+                    );
+            } catch (Conekta_Error $e){
+                throw new Mage_Payment_Model_Info_Exception($e->getMessage());
+            }
             $event->payment->setOxxoExpiryDate($charge->payment_method->expiry_date);
             $event->payment->setOxxoBarcodeUrl($charge->payment_method->barcode_url);
             $event->payment->setChargeId($charge->id);
@@ -102,7 +103,7 @@ class Conekta_Oxxo_Model_Observer{
     public function implementOrderStatus($event)
     {
         $order = $event->getOrder();
-				if ($this->_getPaymentMethod($order) == Mage::getModel('Conekta_Oxxo_Model_Oxxo')->getCode()) {
+		if ($this->_getPaymentMethod($order) == Mage::getModel('Conekta_Oxxo_Model_Oxxo')->getCode()) {
             if ($order->canInvoice())
                 $this->_processOrderStatus($order);
         }
@@ -116,31 +117,8 @@ class Conekta_Oxxo_Model_Observer{
  
     private function _processOrderStatus($order)
     {
-				if ($order->hasInvoices() != true) {
-					$invoice = $order->prepareInvoice();
-					
-					// Check if order is virtual
-					$virtual = false;
-					$items_collection = $order->getItemsCollection(array(), true);
-					for ($i = 0; $i < count($items_collection->getColumnValues('sku')); $i ++) {
-						$product_type = $items_collection->getColumnValues('product_type');
-						if (strcmp($product_type, 'virtual') !== 0) {
-								$virtual = true;
-								break;
-						}
-					}
-					
-					if ($virtual != true) {
-						$invoice->register();
-						Mage::getModel('core/resource_transaction')
-							 ->addObject($invoice)
-							 ->addObject($invoice->getOrder())
-							 ->save();
-					}
-	 
-					$invoice->sendEmail(true, '');
-					$this->_changeOrderStatus($order);
-				}
+        $order->sendNewOrderEmail();
+        $this->_changeOrderStatus($order);
         return true;
     }
  
