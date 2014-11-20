6f131aaba46c5d5b0200888c7f90392f5aee4cdc
MauricioMurga <mauricio_murga@hotmail.com>
2014-11-20 16:20:08 -0600

    add order details on success page

---
+++ app/code/community/Conekta/Realtime/Block/Onepage/Success.php
@@ -0,0 +1,16 @@
+<?php
+class Conekta_Reltime_Block_Onepage_Success extends Mage_Checkout_Block_Onepage_Success
+{
+  // protected function _construct() {
+  //   $order = Mage::getModel('sales/order')->load(Mage::getSingleton('checkout/session')->getLastOrderId());
+  //   $this->setTemplate('checkout/success.phtml');
+  // }
+
+  public function getPayment()
+  {
+      $order = Mage::getModel('sales/order')->loadByIncrementId($this->_getData('order_id'));
+      $payment = $order->getPayment();
+      return $payment;
+  }
+}
+?>
