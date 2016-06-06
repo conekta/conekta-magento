5b676edf5164b47c28c48b37254cb0df1f531195
Mauricio Murga <mauricio@conekta.io>
2016-06-06 10:32:05 -0500

    remove all unnecessary events for webhook creation

--- app/code/community/Conekta/Webhook/Block/Adminhtml/System/Config/Url.php
+++ app/code/community/Conekta/Webhook/Block/Adminhtml/System/Config/Url.php
@@ -29,19 +29,7 @@ class Conekta_Webhook_Block_Adminhtml_System_Config_Url extends Mage_Adminhtml_B
 
     $url->setValue($url_string);
 
-    $events = array("events" =>
-        array("charge.created", "charge.paid", "charge.under_fraud_review",
-        "charge.fraudulent", "charge.refunded", "charge.created", "customer.created",
-        "customer.updated", "customer.deleted", "webhook.created", "webhook.updated",
-        "webhook.deleted", "charge.chargeback.created", "charge.chargeback.updated",
-        "charge.chargeback.under_review", "charge.chargeback.lost", "charge.chargeback.won",
-        "payout.created", "payout.retrying", "payout.paid_out", "payout.failed",
-        "plan.created", "plan.updated", "plan.deleted", "subscription.created",
-        "subscription.paused", "subscription.resumed", "subscription.canceled",
-        "subscription.expired", "subscription.updated", "subscription.paid",
-        "subscription.payment_failed", "payee.created", "payee.updated",
-        "payee.deleted", "payee.payout_method.created",
-        "payee.payout_method.updated", "payee.payout_method.deleted"));
+    $events = array("events" => array("charge.paid"));
     $error = false;
     $error_message = null;
     try {
