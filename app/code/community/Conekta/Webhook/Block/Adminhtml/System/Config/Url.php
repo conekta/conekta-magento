5c871d332af99604b5c4ddb0f15c0df2d9e83614
Mauricio Murga <mauricio@conekta.io>
2016-10-28 11:15:33 -0500

    enhance webhook logic and remove realtime module

--- app/code/community/Conekta/Webhook/Block/Adminhtml/System/Config/Url.php
+++ app/code/community/Conekta/Webhook/Block/Adminhtml/System/Config/Url.php
@@ -32,21 +32,23 @@ class Conekta_Webhook_Block_Adminhtml_System_Config_Url extends Mage_Adminhtml_B
     $events = array("events" => array("charge.paid"), "production_enabled" => 1, "development_enabled" => 1);
     $error = false;
     $error_message = null;
-    try {
-      $different = true;
-      $webhooks = Conekta_Webhook::where();
-      $urls = array();
+    if (!empty(Mage::getStoreConfig('payment/webhook/privatekey'))) {
+      try {
+        $different = true;
+        $webhooks = Conekta_Webhook::where();
+        $urls = array();
 
-      foreach ($webhooks as $webhook) {
-         array_push($urls, $webhook->webhook_url);
+        foreach ($webhooks as $webhook) {
+           array_push($urls, $webhook->webhook_url);
+        }
+
+       if (!in_array($url_string, $urls)){
+          $webhook = Conekta_Webhook::create(array_merge(array("url"=>$url_string), $events));
+       } 
+      } catch(Exception $e) {
+        $error = true;
+        $error_message = $e->getMessage();
       }
-
-     if (!in_array($url_string, $urls)){
-        $webhook = Conekta_Webhook::create(array_merge(array("url"=>$url_string), $events));
-     } 
-    } catch(Exception $e) {
-      $error = true;
-      $error_message = $e->getMessage();
     }
 
     $url->setForm($element->getForm());
